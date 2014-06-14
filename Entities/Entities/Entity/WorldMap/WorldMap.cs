b1d97297814769bd1de023faf5d9f13c4c8f31ae
Matheus Freire <matheusfreire@gmail.com>
2014-06-13 22:42:53 -0300

    Map collision First try
    LET`S SEE!!!! MATTAR NEEDS TO COMMIT THE NEW MAP

--- Entities/Entities/Entity/WorldMap/WorldMap.cs
+++ Entities/Entities/Entity/WorldMap/WorldMap.cs
@@ -25,9 +25,37 @@ namespace Gradius
             m_screenMiddle = new Vector2(m_view.Width, m_view.Height) / 2;
         }
 
-        public override void Update(GameTime gameTime) {
-            m_view.X += Convert.ToInt32(gameTime.ElapsedGameTime.TotalMilliseconds / 32);
-        }
+        public override void Update(GameTime gameTime)
+        {
+            //MAP COLLIDER
+            for (int ol = 0; ol < m_map.ObjectLayers.Count; ol++)
+            {
+                for (int o = 0; o < m_map.ObjectLayers[ol].MapObjects.Length; o++)
+                {
+                    if (m_map.ObjectLayers[ol].MapObjects[o].Polygon != null)
+                    {
+                        Movable mov;
+                        foreach (Entity e in m_world.m_entities)
+                        {
+                            if (e is Player || e is Projectile)
+                            {
+                                mov = (Movable)e;
+                                if (m_map.ObjectLayers[ol].MapObjects[o].Polygon.Intersects(new Rectangle((int)(mov.m_pos.X - mov.m_size.X / 2),
+                                                                                                  (int)(mov.m_pos.Y - mov.m_size.Y / 2),
+                                                                                                  (int)mov.m_size.X,
+                                                                                                  (int)mov.m_size.Y)))
+                                {
+                                    m_world.Remove(mov);
+                                }
+                            }
+                        }
+                    }
+
+                }
+            }
+                m_view.X += Convert.ToInt32(gameTime.ElapsedGameTime.TotalMilliseconds / 30);
+            }
+        
 
         public override void Draw(GameTime gameTime, SpriteBatch spriteBatch) {
             m_map.Draw(spriteBatch, m_view);
